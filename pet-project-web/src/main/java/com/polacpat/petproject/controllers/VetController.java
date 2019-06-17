605b4edde5883f119311cde2fe6be3cbadbc3a33
Veranicus <polacek.patrik@gmail.com>
2019-06-17 17:14:09 +0200

    Added list of vets and vet controller. Closes #8

--- pet-project-web/src/main/java/com/polacpat/petproject/controllers/VetController.java
+++ pet-project-web/src/main/java/com/polacpat/petproject/controllers/VetController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping
 public class VetController {
     @GetMapping({"/vets", "vets/index", "vets/index.html"})
     public String listVets() {
